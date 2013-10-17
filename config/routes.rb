27f3d015aa0c71489e5d80dd7e1ad9a56a9c88d8
Robert Grzesik <rgrzesik@dresourcesgroup.com>
2013-10-17 17:03:23 -0400

    bootstrap, devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ServerTest::Application.routes.draw do
+  devise_for :users
   get "main/index"
   get "main/about"
   # The priority is based upon order of creation: first created -> highest priority.
